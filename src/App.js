a711842f9bdda8a6931a09c3fa25f46450d6030b
HongKing896 <butyking7896@gmail.com>
2025-03-08 19:12:16 +0900

    홍찬 로컬에서 작동함작

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import MyRoutes from "./Routes";
 import GlobalStyle from './styles/GlobalStyle';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
-import SvgIcon from "@mui/material/SvgIcon";
-import { SvgIconComponent } from "@mui/icons-material";
 // styles
-import"./asset/css/paper-kit.css"
+import "./asset/css/paper-kit.css";
 // import "bootstrap/scss/bootstrap.scss";
 // import "assets/scss/paper-kit.scss?v=1.3.0";
 // import "assets/demo/demo.css?v=1.3.0";
@@ -18,7 +16,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <BrowserRouter>
+      <BrowserRouter basename="/hisign">
         <RecoilRoot>
           <MyRoutes />
         </RecoilRoot>
