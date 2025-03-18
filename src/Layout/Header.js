9d64050d06ff903de66a507222b55b9524dd4aa2
thisissolmi <oksk6685@naver.com>
2025-03-18 19:23:58 +0900

    add user name

--- src/Layout/Header.js
+++ src/Layout/Header.js
@@ -3,9 +3,11 @@ import { useRecoilValue } from "recoil";
 import HisnetLoginButton from "../components/HisnetLoginButton";
 import HisnetLogoutButton from "../components/HisnetLogoutButton";
 import { authState } from "../recoil/atom/authState";
+import { loginMemberState } from "../recoil/atom/loginMemberState"; // 로그인한 사용자 정보 가져오기
 
 function HeaderBar() {
     const auth = useRecoilValue(authState);
+    const loginMember = useRecoilValue(loginMemberState); // 로그인한 멤버 정보
 
     return (
         <div className="d-flex flex-column w-100">
@@ -23,13 +25,14 @@ function HeaderBar() {
                     zIndex: 1000
                 }}
             >
-               <Link to="/" className="text-decoration-none text-dark fs-4 ms-3">
-                  <img
-                      src={`${process.env.PUBLIC_URL}/hi-sign-logo.png`}
-                      alt="HI-Sign 로고"
-                      style={{ height: "80px" }}
-                  />
-              </Link>
+                <Link to="/" className="text-decoration-none text-dark fs-4 ms-3">
+                    <img
+                        src={`${process.env.PUBLIC_URL}/hi-sign-logo.png`}
+                        alt="HI-Sign 로고"
+                        style={{ height: "80px" }}
+                    />
+                </Link>
+                
                 {auth.isAuthenticated && (
                     <div className="d-flex gap-3">
                         <Link to="/request-document" className="nav-link text-dark">
@@ -47,21 +50,23 @@ function HeaderBar() {
                         <Link to="/align" className="nav-link text-dark">
                             서명 할당하기
                         </Link>
-                       
                         <Link to="/contact" className="nav-link text-dark">
                             문의 페이지
                         </Link>
-                        
                     </div>
                 )}
 
                 <div className="d-flex align-items-center gap-3 me-3">
+                    {auth.isAuthenticated && loginMember.name ? (
+                        <span className="fw-bold text-dark">{loginMember.name}님, 반갑습니다!</span>
+                    ) : null}
+
                     {auth.isAuthenticated ? <HisnetLogoutButton /> : <HisnetLoginButton />}
                 </div>
             </header>
 
             <div className="w-100" style={{ marginTop: "50px" }}>
-                <Outlet/>
+                <Outlet />
             </div>
         </div>
     );
