87c966d164bf495c55aacdcebae3d4ed4de9fda2
HongKing896 <butyking7896@gmail.com>
2025-03-26 17:47:20 +0900

    chore:사용자 로그인 상태 관리 수정- LandingPage

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,17 +1,11 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import styled from "styled-components";
 import HisnetLoginButton from "../components/HisnetLoginButton";
-import { authState } from "../recoil/atom/authState";
+import { loginMemberState } from "../recoil/atom/loginMemberState";
 
 function SideBar() {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-    const auth = useRecoilValue(authState);
-
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
+    const loginMember = useRecoilValue(loginMemberState);
 
     return (
 
@@ -20,7 +14,7 @@ function SideBar() {
                 <h2> 쉽고 빠르게 안전한 전자서명 솔루션 </h2>
                 <h1> Hi-Sign </h1>
                 <br></br>
-                {auth.isAuthenticated ? (
+                { !!loginMember.unique_id ? (
                     <CenterLink to="/tasksetup">서명 요청 시작하기</CenterLink>
                 ) : (
                     <HisnetLoginButton>히즈넷으로 로그인</HisnetLoginButton>
@@ -30,7 +24,6 @@ function SideBar() {
     );
 
 }
-
 export default SideBar;
 
 const CenterContainer = styled.div`
@@ -42,66 +35,6 @@ const CenterContainer = styled.div`
     flex-direction: column;
     align-items: center;
     gap: 20px;
-`;
-
-const SidebarContainer = styled.div.withConfig({
-    shouldForwardProp: (prop) => prop !== 'isOpen'
-}).attrs(props => ({
-    'aria-hidden': !props.isOpen
-}))`
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    width: ${({ isOpen }) => (isOpen ? "200px" : "0")};
-    padding: ${({ isOpen }) => (isOpen ? "20px" : "0")};
-    background-color: white;
-    border-right: 1px solid grey;
-    transition: width 0.3s ease, padding 0.3s ease;
-    overflow: hidden;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 1000;
-`;
-
-const HamburgerButton = styled.div`
-    display: block;
-    font-size: 30px;
-    cursor: pointer;
-    position: fixed; /* 화면 고정 */
-    top: 5px; /* 위쪽 위치 */
-    left: 20px; /* 왼쪽 위치 */
-    color: black;
-    z-index: 2000;
-`;
-
-const NavigationLinks = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 15px;
-    flex-grow: 1;
-`;
-
-const StyledLink = styled(Link)`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    text-decoration: none;
-    color: white;
-    background-color: grey;
-    border-radius: 50px;
-    padding: 10px 20px;
-    font-size: 14px;
-    font-weight: bold;
-    transition: background-color 0.3s ease;
-
-    &:hover {
-        background-color: #0056b3;
-    }
-
-    &:active {
-        background-color: #004494;
-    }
 `;
 
 const CenterLink = styled(Link)`
