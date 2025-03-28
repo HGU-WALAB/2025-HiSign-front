39809772d082bfe89b41c9dc939353289faac41c
HongKing896 <butyking7896@gmail.com>
2025-03-28 15:54:28 +0900

    fix: authState 삭제

--- src/recoil/atom/authState.js
+++
@@ -1,8 +0,0 @@
-import { atom } from 'recoil';
-
-export const authState = atom({
-  key: 'authState',
-  default: {
-    isAuthenticated: !!sessionStorage.getItem('token'),  // 토큰이 존재하는지 여부로 로그인 상태 결정
-  },
-});
